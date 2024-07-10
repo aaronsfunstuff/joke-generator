829f3ef84b39aa993ccdff3e3f100d1402472d86
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-10 15:59:09 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,6 +4,11 @@ const jokes = [
     "Why don't skeletons fight each other? They don't have the guts.",
     "What do you call fake spaghetti? An impasta.",
     "How does a penguin build its house? Igloos it together."
+    "Parallel lines have so much in common. It’s a shame they’ll never meet.",
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "I'm reading a book on anti-gravity. It's impossible to put down!",
+    "Why don’t some couples go to the gym? Because some relationships don’t work out.",
+    "What did one wall say to the other wall? 'I'll meet you at the corner!'"
 ];
 
 function generateJoke() {
